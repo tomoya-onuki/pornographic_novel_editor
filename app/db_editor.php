0fef47464085475888afaf74529756655ef95e56
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-02-19 17:02:51 +0900

    db

---
+++ app/db_editor.php
@@ -0,0 +1,32 @@
+<?php
+
+// データベースに接続
+$url = parse_url(getenv('DATABASE_URL'));
+$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+$pdo = new PDO($dsn, $url['user'], $url['pass']);
+
+$targetID = 0;
+
+// PDOでDBからデータを取得
+$stmt = $pdo->prepare('SELECT * FROM dict WHERE id = :id');
+$stmt->bindParam(':id', $targetID, PDO::PARAM_STR);
+$stmt->execute();
+
+?>
+
+<table>
+    <tr>
+        <th>用語</th>
+        <th>意味</th>
+        <th>用法</th>
+        <th>作者</th>
+    </tr>
+    <?php while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+        <tr>
+            <th>{$result['word']}</th>
+            <th>{$result['meaning']}</th>
+            <th>{$result['ex_sentence']}</th>
+            <th>{$result['author']}</th>
+        </tr>
+    <?php } ?>
+</table>
