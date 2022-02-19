77f88268218569abba7be67c30a3dbbb2e7ebce8
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-02-19 17:37:48 +0900

    db

--- app/main.js
+++ app/main.js
@@ -3,15 +3,20 @@ $(function () {
 
     $('#db_add_btn').click(function () {
         var hostUrl = 'db_add.php';
+        // $.post('db_add.php', 
+        // {
+        //     "task_title": $(this).val(),
+        //     "id": $('#task_id').val()
+        // }
         $.ajax({
             url: hostUrl,
             type: 'POST',
             dataType: 'json',
             data: {
-                word: $('#word').val(),
-                meaning: $('#meaning').val(),
-                ex_sentence: $('#ex_sentence').val(),
-                author: $('#author').val()
+                'word': $('#word').val(),
+                'meaning': $('#meaning').val(),
+                'ex_sentence': $('#ex_sentence').val(),
+                'author': $('#author').val()
             },
             timeout: 3000,
         }).done(function (data) {
