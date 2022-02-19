4f1b2710c62d638343d10a29aac0cf4476f9ffc9
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-02-20 03:53:47 +0900

    db

--- app/top.php
+++ app/top.php
@@ -139,7 +139,7 @@ if (!empty($_POST['key'])) {
         <div class="draft_ellipse ellipse"></div>
         <button id="update">更新</button>
 
-        <button id="draft" onclick="loction.href='./story.php?key=<?=$_POST['key']?>'">入稿</button>
+        <button id="draft" onclick="location.href='./story.php?key=<?=$_POST['key']?>'">入稿</button>
         <!-- <form action="./story.php" method="get">
             <input id="draft" name="draft" value="入稿">
             <input type="hidden" name="key" value="">
