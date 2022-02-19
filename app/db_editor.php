3b50564022afaad2e46c406ae27d134a968904b5
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-02-20 04:10:43 +0900

    db

--- app/db_editor.php
+++ app/db_editor.php
@@ -24,7 +24,6 @@ function getDB($pdo)
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="style.css">
     <!-- <script src="./main.js"></script> -->
     <title>データベース編集</title>
 </head>
