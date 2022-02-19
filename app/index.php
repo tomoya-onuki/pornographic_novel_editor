202b16359e3911747768ff1bc47ba311d4172bc9
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-02-19 22:36:30 +0900

    db

--- app/index.php
+++ app/index.php
@@ -45,10 +45,11 @@ function random_keyword()
     $keyword = random_keyword();
     $sentence = '';
     $editor = 1;
-    $stmt = $pdo->prepare('INSERT INTO script (key, sentence, editor, word) VALUES (:key, :sentence, :editor, word)');
+    $stmt = $pdo->prepare('INSERT INTO script (key, sentence, editor, word) VALUES (:key, :sentence, :editor, :word)');
     $stmt->bindParam(':key', $keyword, PDO::PARAM_STR);
     $stmt->bindParam(':sentence', $sentence, PDO::PARAM_STR);
     $stmt->bindParam(':editor', $editor, PDO::PARAM_INT);
+    $stmt->bindParam(':word', 'word', PDO::PARAM_STR);
     $stmt->execute();
     ?>
 
