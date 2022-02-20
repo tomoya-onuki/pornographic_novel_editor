3a20468b2d3c00af9bd4ac7104225641dd79aef3
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-02-20 14:15:37 +0900

    color

--- app/index.php
+++ app/index.php
@@ -78,7 +78,7 @@ foreach (glob('./img/{*.jpg}', GLOB_BRACE) as $file) {
             <div class="flex_item">
                 <a href="./story.php?key=<?= $result['key'] ?>">
                     <div class="story_mask" style="background:<?=$result['color']?>"></div>
-                    <img class="story_img" src="<?=$img_array[$idx]?>" alt="">
+                    <img class="story_img" style="background:<?=$result['color']?>" src="<?=$img_array[$idx]?>" alt="">
                     <div class="story_title"><?= $result['word'] ?></div>
                 </a>
             </div>
