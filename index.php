f36b7ee07421fd1d928234abeae33b2f51f0adf3
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-02-20 17:57:54 +0900

    hoge

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
 <?php
-
+header("Location: ./app/");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SAMPLE</title>
-</head>
-<body>
-    
-<div><a href="./app01/">app01 : スマホをジャイロセンサーにするプログラム</a></div>
-<div><a href="./app02/">app02 : シミュレーションゲームの雛形</a></div>
-<div><a href="./app03/">app03 : 顔認識（姿勢, 表情の識別）</a></div>
-</body>
-</html>
