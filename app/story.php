e69a59bc43e143ebba091af43cbbd413f688f4e8
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-02-20 11:44:49 +0900

    db

--- app/story.php
+++ app/story.php
@@ -14,6 +14,7 @@ $pdo = new PDO($dsn, $url['user'], $url['pass']);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <title>ふたりで書く官能小説</title>
 
     <script>
