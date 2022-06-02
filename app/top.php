e37ac05269c5f4093eb18bf4632d4e62e2fe31d4
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-06-02 21:08:56 +0900

    ユーザー交代の仕組みを変更

--- app/top.php
+++ app/top.php
@@ -148,7 +148,7 @@ if (!empty($_GET['key'])) {
                             'line': line + 1
                         },
                         function(data) {
-                            console.log(data);
+                            // console.log(data);
                             check_editor(data.editor); // エディタがどちらか判定
                             $('#script').html(data.sentence);
                             line = data.line;
@@ -165,7 +165,7 @@ if (!empty($_GET['key'])) {
             
             // DBの監視
             function check_db() {
-                // console.log('check data base')
+                console.log('check data base')
                 $.post('check.php', {
                         'key': '<?= $_GET['key'] ?>'
                     },
